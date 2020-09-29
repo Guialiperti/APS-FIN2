3f7831ba3c4891a667d517b47133eee5a4c163ac
Guilherme Silvarolli Aliperti <gui.aliperti9@gmail.com>
2020-09-28 22:39:29 -0300

    Parcialmente finalizado

--- portfolio.py
+++ portfolio.py
@@ -9,7 +9,6 @@ from pypfopt import CLA
 # Read in price data
 df = pd.read_csv("dados/grupo1.csv", parse_dates=True, index_col="Data")
 
-i = 1
 max_return = 0
 for i in range(1,11):
     path = "dados/grupo" + str(i) + ".csv"
@@ -22,7 +21,6 @@ for i in range(1,11):
     ef = EfficientFrontier(mu, S)
     raw_weights = ef.max_sharpe()
     cleaned_weights = ef.clean_weights()
-    ef.save_weights_to_file("weights.csv")  # saves to file
     print(cleaned_weights)
     ef.portfolio_performance(verbose=True)
 
@@ -34,6 +32,7 @@ for i in range(1,11):
     if retorno[0] > max_return:
         max_return = retorno[2]
         port = path
+        ef.save_weights_to_file("weights.csv")  # saves to file
 
 
 print(max_return)
